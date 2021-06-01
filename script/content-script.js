545dc73a634cc44f501e82257f882f83ca920578
kingcos <2821836721v@gmail.com>
2021-06-02 01:02:22 +0800

    Add observe logics

--- script/content-script.js
+++ script/content-script.js
@@ -41,3 +41,61 @@ function filterRows(filterInputValue, targetClass) {
 //     console.log(request.action)
 //   }
 // )
+
+function tableAdded(table) {
+  console.log('tableAdded')
+
+  findAllTables(table)
+}
+
+function trAdded(tr) {
+  console.log('trAdded')
+
+}
+
+function findAllTables(table) {
+  var tables
+  if (table != null) {
+    tables = [table]
+  } else {
+    tables = document.getElementsByTagName('table')
+  }
+
+  for (var i = 0; i < tables.length; i += 1) {
+    tables[i].style.background = 'red'
+  }
+}
+
+function startObserveWebPage() {
+  console.log('startObserveWebPage')
+
+  // 找已加载表格
+  findAllTables(null)
+
+  // 监听是否有表格
+  let target = document.getElementsByTagName('body')[0]
+  let options = { childList: true, subtree: true }
+  let observer = new MutationObserver((mutations) => {
+    // console.log(mutations)
+    for (var i = 0; i < mutations.length; i += 1) {
+      var mutation = mutations[i]
+
+      for (var j = 0; j < mutation.addedNodes.length; j += 1) {
+        var addedNode = mutation.addedNodes[j]
+
+        if (addedNode.nodeName.toLowerCase() == 'table') {
+          tableAdded(addedNode)
+        }
+
+        if (addedNode.nodeName.toLowerCase() == 'tr') {
+          trAdded(addedNode)
+        }
+      }
+    }
+
+  })
+
+  observer.observe(target, options)
+}
+
+window.onload = startObserveWebPage
